f0aaf483def77d370e7d70883b05e1b4a494d635
Nadiia1701 <146233511+Nadiia1701@users.noreply.github.com>
2024-04-21 01:43:10 -0400

    Update ImageCard.jsx

--- src/components/ImageCard/ImageCard.jsx
+++ src/components/ImageCard/ImageCard.jsx
@@ -1,8 +1,12 @@
 import css from "./ImageCard.module.css"
 
-export default function ImageCard({ urls, description }) {
+export default function ImageCard({ urls, description, onClick }) {
+    const handleClick = () => {
+        onClick(urls.small, urls.regular);
+    };
+
     return (
-        <div>
+        <div onClick={handleClick}>
             <img className={css.img} src={urls.small} alt={description} />
         </div>
     );
